c4edb3e6c6a16b090208a03bf6d57349dd78343c
coolwli <acandar013@gmail.com>
2024-04-22 14:36:51 +0300

    update

---
+++ show-datas-from-excel/show-datas-from-excel/default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="WebApplication1._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        .panel {
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            margin-bottom: 20px;
+            padding: 20px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+
+        .panel h1 {
+            color: #333;
+            margin-top: 0;
+        }
+
+        .panel p {
+            color: #666;
+            margin-bottom: 0;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="container" runat="server" class="container">
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ show-datas-from-excel/show-datas-from-excel/default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using OfficeOpenXml;
+
+
+namespace WebApplication1
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoadDataFromExcel();
+        }
+
+        protected void LoadDataFromExcel()
+        {
+            string excelFilePath = @"D:\excel.xlsx";
+            FileInfo fileInfo = new FileInfo(excelFilePath);
+
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.Commercial;
+
+            using (ExcelPackage package = new ExcelPackage(fileInfo))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+
+                int rowCount = worksheet.Dimension.Rows;
+
+                for (int row = 1; row <= rowCount; row++) 
+                {
+                    string title = worksheet.Cells[row, 1].Value.ToString();
+                    string content = worksheet.Cells[row, 2].Value.ToString();
+
+                    Panel panel = new Panel();
+                    panel.CssClass = "panel";
+
+                    LiteralControl heading = new LiteralControl("<h1>" + title + "</h1>");
+                    panel.Controls.Add(heading);
+
+                    LiteralControl paragraph = new LiteralControl("<p>" + content + "</p>");
+                    panel.Controls.Add(paragraph);
+
+                    container.Controls.Add(panel);
+                }
+            }
+        }
+    }
+}
